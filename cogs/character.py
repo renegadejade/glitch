6c59e9f999347fb8f1944a64ef79de2ff0ea5d74
John Thomas <jt@johnthomas.tech>
2020-12-16 19:00:35 -0500

    Update character.py

--- cogs/character.py
+++ cogs/character.py
@@ -14,6 +14,27 @@ class Character(commands.Cog):
     async def character(self, ctx):
         await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help character` for help.")
 
+    @character.command(name="new", help="Creates a new character.")
+    async def new(self, ctx, name):
+        data = {
+            "name" : name
+        }
+        characters = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").stream()
+        for character in characters:
+            if character.to_dict()["name"] == name:
+                results = "You already have a character with that name. Use `!character load [name]` to switch to them."
+                await ctx.send(results)
+                return
+
+        DB.collection("users").document(str(ctx.message.author.id)).collection("characters").add(data)
+        character_id = ""
+        characters = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").stream()
+        for character in characters:
+            if character.to_dict()["name"] == name:
+                character_id = character.id
+        DB.collection("users").document(str(ctx.message.author.id)).set({"active" : character_id})
+        results = "Your new character, **" + name + "** is loaded and ready to go!"
+        await ctx.send(results)
 
     @character.command(name="name", help="Set your character's name")
     async def name(self, ctx, name):
@@ -21,11 +42,12 @@ class Character(commands.Cog):
             "name" : name
         }
         DB.collection("users").document(str(ctx.message.author.id)).set(data, merge=True)
-        results = "Your character's name is " + name
+        results = "Your character's new name is " + name + "."
         await ctx.send(results)
 
     @character.command(name="set", help="Set a STAT or Skill to a certain level")
     async def set(self, ctx, statorskill, val):
+        active = DB.collection("users").document(str(ctx.message.author.id)).get().to_dict()["active"]
         stats_stream = DB.collection("stats").stream()
         skills_stream = DB.collection("skills").stream()
         
@@ -38,8 +60,8 @@ class Character(commands.Cog):
             skills.append(skill.id)
 
         print(stats)
-        if statorskill.lower() in stats and int(val) is not None:
-            character_ref = DB.collection("users").document(str(ctx.message.author.id))
+        if statorskill.lower() in stats or skills and int(val) is not None:
+            character_ref = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active)
             character_ref.set(
                 {
                     statorskill.lower() : val
@@ -47,15 +69,15 @@ class Character(commands.Cog):
             )
             character = character_ref.get()
             results = character.to_dict()["name"]+ "'s " + statorskill.capitalize() + " is now set to " + str(val)
-        elif statorskill.lower() in skills and int(val) is not None:
-            character_ref = DB.collection("users").document(str(ctx.message.author.id))
-            character_ref.set(
-                {
-                    statorskill.lower() : val
-                }, merge=True
-            )
-            character = character_ref.get()
-            results = character.to_dict()["name"]+ "'s " + statorskill.capitalize() + " is now set to " + str(val)
+        # elif statorskill.lower() in skills and int(val) is not None:
+        #     character_ref = DB.collection("users").document(str(ctx.message.author.id)).collection("character").document(active)
+        #     character_ref.set(
+        #         {
+        #             statorskill.lower() : val
+        #         }, merge=True
+        #     )
+        #     character = character_ref.get()
+        #     results = character.to_dict()["name"]+ "'s " + statorskill.capitalize() + " is now set to " + str(val)
         else:
             results = "\"" + statorskill + "\" isn't a STAT or Skill. Try again."
         
