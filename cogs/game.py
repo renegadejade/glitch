e043c2ed15f86f6ebe3c77354208db6956bca687
John Thomas <jt@johnthomas.tech>
2020-12-17 16:51:39 -0500

    added luck modifier flag

--- cogs/game.py
+++ cogs/game.py
@@ -13,7 +13,8 @@ class Game(commands.Cog):
         await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help game` for help.")
 
     @game.command(name="skillcheck", aliases=["sc"], help="Rolls a skill check.")
-    async def skillcheck(self, ctx, skillStr):
+    async def skillcheck(self, ctx, skillStr, *args):
+        print(args)
         active = DB.collection("users").document(str(ctx.message.author.id)).get().to_dict()["active"]
         character = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active).get().to_dict()
 
@@ -38,12 +39,32 @@ class Game(commands.Cog):
             results = "**" + skillStr + "** is not a skill. Try again."
             await ctx.send(results)
             return
-        
+        luck = 0
+        if "-l" in args:
+            flag_position = args.index("-l")
+            luckStr = str(args[flag_position + 1])
+            if luckStr.isnumeric():
+                luck = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the -l flag."
+                await ctx.send(results)
+                return
+            print(luck)
+        elif "--luck" in args:
+            flag_position = args.index("--luck")
+            luckStr = args[flag_position + 1]
+            if luckStr.isnumeric():
+                luck = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the --luck flag."
+                await ctx.send(results)
+                return
+
         roll = random.randint(1,10)
-        total = skill_level + stat_level + roll
+        total = skill_level + stat_level + roll + luck
 
         results = character["name"] + " rolled a " + str(roll) + " for a total result of: **" + str(total) + "**"
-        results += "\n" + stat.capitalize() + ": " + str(stat_level) + "|" + skillStr.capitalize() + ": " + str(skill_level) + "|Roll: " + str(roll)
+        results += "\n" + stat.capitalize() + ": " + str(stat_level) + "|" + skillStr.capitalize() + ": " + str(skill_level) + "|Roll: " + str(roll) + "|LUCK: " + str(luck)
         if roll == 10:
             crit = random.randint(1,10)
             total += crit
