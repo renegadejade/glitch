d3e1b8a78150ff5e2335cec6f58edeecf5995801
Frank Pasqualini <FJP11@pitt.edu>
2021-03-09 23:07:06 -0500

    Reset cogs

--- cogs/character.py
+++ cogs/character.py
@@ -9,136 +9,150 @@ from data import DB
 
 
 class Character(commands.Cog):
-	"""
-	TODO
-	"""
-	def __init__(self, bot):
-		self.bot = bot
+    """
+    TODO
+    """
 
-	@commands.group(aliases=["c", "char"], invoke_without_command=True)
-	async def character(self, ctx):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		"""
-		await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help character` for help.")
+    def __init__(self, bot):
+        self.bot = bot
 
-	@character.command(name="new", help="Creates a new character.")
-	async def new(self, ctx, name):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		:param name:
-		:type name:
-		:return:
-		:rtype:
-		"""
-		data = {"name": name}
-		characters = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").stream()
-		for character in characters:
-			if character.id == name:
-				results = "You already have a character named **" + name + "**. Use `!character load " + name + "` to switch to them."
-				await ctx.send(results)
-				return
-		DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(name).set(data)
-		DB.collection("users").document(str(ctx.message.author.id)).set({"active": name})
-		results = "Your new character, **" + name + "**, is loaded and ready to go!"
-		await ctx.send(results)
+    @commands.group(aliases=["c", "char"], invoke_without_command=True)
+    async def character(self, ctx):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        """
+        await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help character` for help.")
 
-	@character.command(name="load", help="Loads a character and makes them active.")
-	async def load(self, ctx, name):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		:param name:
-		:type name:
-		:return:
-		:rtype:
-		"""
-		characters = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").stream()
-		for character in characters:
-			if character.id == name:
-				DB.collection("users").document(str(ctx.message.author.id)).set({"active": name})
-				results = "**" + name + "** is loaded and ready to go!"
-				await ctx.send(results)
-				return
+    @character.command(name="new", help="Creates a new character.")
+    async def new(self, ctx, name):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        :param name:
+        :type name:
+        :return:
+        :rtype:
+        """
+        data = {"name": name}
+        characters = DB.collection("users").document(
+            str(ctx.message.author.id)).collection("characters").stream()
+        for character in characters:
+            if character.id == name:
+                results = "You already have a character named **" + name + \
+                    "**. Use `!character load " + name + "` to switch to them."
+                await ctx.send(results)
+                return
+        DB.collection("users").document(str(ctx.message.author.id)).collection(
+            "characters").document(name).set(data)
+        DB.collection("users").document(
+            str(ctx.message.author.id)).set({"active": name})
+        results = "Your new character, **" + name + "**, is loaded and ready to go!"
+        await ctx.send(results)
 
-		results = "**" + name + "** isn't one of your characters. To create them type `!character new " + name + "`."
-		await ctx.send(results)
+    @character.command(name="load", help="Loads a character and makes them active.")
+    async def load(self, ctx, name):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        :param name:
+        :type name:
+        :return:
+        :rtype:
+        """
+        characters = DB.collection("users").document(
+            str(ctx.message.author.id)).collection("characters").stream()
+        for character in characters:
+            if character.id == name:
+                DB.collection("users").document(
+                    str(ctx.message.author.id)).set({"active": name})
+                results = "**" + name + "** is loaded and ready to go!"
+                await ctx.send(results)
+                return
 
-	@character.command(name="set", help="Set a STAT or Skill to a certain level")
-	async def set(self, ctx, stat_or_skill, val):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		:param stat_or_skill:
-		:type stat_or_skill:
-		:param val:
-		:type val:
-		:return:
-		:rtype:
-		"""
-		active = DB.collection("users").document(str(ctx.message.author.id)).get().to_dict()["active"]
-		stats_stream = DB.collection("stats").stream()
-		skills_stream = DB.collection("skills").stream()
+        results = "**" + name + \
+            "** isn't one of your characters. To create them type `!character new " + name + "`."
+        await ctx.send(results)
 
-		stats = []
-		skills = []
+    @character.command(name="set", help="Set a STAT or Skill to a certain level")
+    async def set(self, ctx, stat_or_skill, val):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        :param stat_or_skill:
+        :type stat_or_skill:
+        :param val:
+        :type val:
+        :return:
+        :rtype:
+        """
+        active = DB.collection("users").document(
+            str(ctx.message.author.id)).get().to_dict()["active"]
+        stats_stream = DB.collection("stats").stream()
+        skills_stream = DB.collection("skills").stream()
 
-		for stat in stats_stream:
-			stats.append(stat.id)
-		for skill in skills_stream:
-			skills.append(skill.id)
+        stats = []
+        skills = []
 
-		if val is None or val.isnumeric() is False:
-			results = "Please enter a number after the STAT or Skill. Try again."
-			await ctx.send(results)
-			return
+        for stat in stats_stream:
+            stats.append(stat.id)
+        for skill in skills_stream:
+            skills.append(skill.id)
 
-		if stat_or_skill.lower() in stats or stat_or_skill.lower() in skills:
-			character_ref = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active)
-			character_ref.set({stat_or_skill.lower(): val}, merge=True)
+        if val is None or val.isnumeric() is False:
+            results = "Please enter a number after the STAT or Skill. Try again."
+            await ctx.send(results)
+            return
 
-			results = "**" + active + "'s** *" + stat_or_skill.capitalize() + "* is now set to " + str(val) + "."
+        if stat_or_skill.lower() in stats or stat_or_skill.lower() in skills:
+            character_ref = DB.collection("users").document(
+                str(ctx.message.author.id)).collection("characters").document(active)
+            character_ref.set({stat_or_skill.lower(): val}, merge=True)
 
-		else:
-			results = "\"" + stat_or_skill + "\" isn't a STAT or Skill. Try again."
+            results = "**" + active + "'s** *" + \
+                stat_or_skill.capitalize() + "* is now set to " + str(val) + "."
 
-		await ctx.send(results)
+        else:
+            results = "\"" + stat_or_skill + "\" isn't a STAT or Skill. Try again."
 
-	@character.command(name="view", help="View information about your character.")
-	async def view(self, ctx):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		"""
-		active = DB.collection("users").document(str(ctx.message.author.id)).get().to_dict()["active"]
-		character = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active).get().to_dict()
+        await ctx.send(results)
 
-		embed = discord.Embed(title=character["name"], description="Netrunner", color=0xff69b4)
-		embed.set_thumbnail(url="https://i.imgur.com/cRIB0hg.jpg")
-		embed.add_field(name="INT", value=character["intelligence"])
-		embed.add_field(name="REF", value=character["reflexes"])
-		embed.add_field(name="DEX", value=character["dexterity"])
-		embed.add_field(name="TECH", value=character["technique"])
-		embed.add_field(name="COOL", value=character["cool"])
-		embed.add_field(name="WILL", value=character["willpower"])
-		embed.add_field(name="LUCK", value=character["luck"])
-		embed.add_field(name="MOVE", value=character["movement"])
-		embed.add_field(name="BODY", value=character["body"])
-		embed.add_field(name="EMP", value=character["empathy"])
-		await ctx.send(embed=embed)
+    @character.command(name="view", help="View information about your character.")
+    async def view(self, ctx):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        """
+        active = DB.collection("users").document(
+            str(ctx.message.author.id)).get().to_dict()["active"]
+        character = DB.collection("users").document(str(ctx.message.author.id)).collection(
+            "characters").document(active).get().to_dict()
+
+        embed = discord.Embed(
+            title=character["name"], description="Netrunner", color=0xff69b4)
+        embed.set_thumbnail(url="https://i.imgur.com/cRIB0hg.jpg")
+        embed.add_field(name="INT", value=character["intelligence"])
+        embed.add_field(name="REF", value=character["reflexes"])
+        embed.add_field(name="DEX", value=character["dexterity"])
+        embed.add_field(name="TECH", value=character["technique"])
+        embed.add_field(name="COOL", value=character["cool"])
+        embed.add_field(name="WILL", value=character["willpower"])
+        embed.add_field(name="LUCK", value=character["luck"])
+        embed.add_field(name="MOVE", value=character["movement"])
+        embed.add_field(name="BODY", value=character["body"])
+        embed.add_field(name="EMP", value=character["empathy"])
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
-	"""
-	TODO
-	:param bot:
-	:type bot:
-	"""
-	bot.add_cog(Character(bot))
+    """
+    TODO
+    :param bot:
+    :type bot:
+    """
+    bot.add_cog(Character(bot))
--- cogs/game.py
+++ cogs/game.py
@@ -10,118 +10,127 @@ from data import DB, SKILLS
 
 
 class Game(commands.Cog):
-	"""
-	TODO
-	"""
-	def __init__(self, bot):
-		self.bot = bot
+    """
+    TODO
+    """
 
-	@commands.group(aliases=["g"], invoke_without_command=True)
-	async def game(self, ctx):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		"""
-		await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help game` for help.")
+    def __init__(self, bot):
+        self.bot = bot
 
-	@game.command(name="skill_check", aliases=["sc"], help="Rolls a skill check.")
-	async def skill_check(self, ctx, skill_str, *args):
-		"""
-		TODO
-		:param ctx:
-		:type ctx:
-		:param skill_str:
-		:type skill_str:
-		:param args:
-		:type args:
-		:return:
-		:rtype:
-		"""
-		print(args)
-		active = DB.collection("users").document(str(ctx.message.author.id)).get().to_dict()["active"]
-		character = DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active).get().to_dict()
+    @commands.group(aliases=["g"], invoke_without_command=True)
+    async def game(self, ctx):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        """
+        await ctx.send(f"Incorrect usage. Use `{ctx.prefix}help game` for help.")
 
-		stat = ""
-		skill_exists = False
+    @game.command(name="skill_check", aliases=["sc"], help="Rolls a skill check.")
+    async def skill_check(self, ctx, skill_str, *args):
+        """
+        TODO
+        :param ctx:
+        :type ctx:
+        :param skill_str:
+        :type skill_str:
+        :param args:
+        :type args:
+        :return:
+        :rtype:
+        """
+        print(args)
+        active = DB.collection("users").document(
+            str(ctx.message.author.id)).get().to_dict()["active"]
+        character = DB.collection("users").document(str(ctx.message.author.id)).collection(
+            "characters").document(active).get().to_dict()
 
-		for skill in SKILLS:
-			if skill["name"] == skill_str:
-				stat = skill["stat"]
-				skill_exists = True
+        stat = ""
+        skill_exists = False
 
-		if skill_exists and skill_str in character:
-			skill_level = int(character[skill_str])
-			stat_level = int(character[stat])
-		elif skill_exists:
-			DB.collection("users").document(str(ctx.message.author.id)).collection("characters").document(active).set({skill_str: 0}, merge=True)
-			skill_level = 0
-			stat_level = int(character[stat])
-		else:
-			results = "**" + skill_str + "** is not a skill. Try again."
-			await ctx.send(results)
-			return
-		luck = 0
-		if "-l" in args:
-			flag_position = args.index("-l")
-			luck_str = str(args[flag_position + 1])
-			if luck_str.isnumeric():
-				luck = int(args[flag_position + 1])
-			else:
-				results = "Please enter a number after the -l flag."
-				await ctx.send(results)
-				return
-			print(luck)
-		elif "--luck" in args:
-			flag_position = args.index("--luck")
-			luck_str = args[flag_position + 1]
-			if luck_str.isnumeric():
-				luck = int(args[flag_position + 1])
-			else:
-				results = "Please enter a number after the --luck flag."
-				await ctx.send(results)
-				return
-		mod = 0
-		if "-m" in args:
-			flag_position = args.index("-m")
-			mod_str = str(args[flag_position + 1])
-			if mod_str.lstrip("-").isnumeric():
-				mod = int(args[flag_position + 1])
-			else:
-				results = "Please enter a number after the -m flag."
-				await ctx.send(results)
-				return
-			print(mod)
-		elif "--mod" in args:
-			flag_position = args.index("--mod")
-			mod_str = args[flag_position + 1]
-			if mod_str.lstrip("-").isnumeric():
-				mod = int(args[flag_position + 1])
-			else:
-				results = "Please enter a number after the --mod flag."
-				await ctx.send(results)
-				return
-		roll = random.randint(1, 10)
-		total = skill_level + stat_level + roll + luck + mod
+        for skill in SKILLS:
+            if skill["name"] == skill_str:
+                stat = skill["stat"]
+                skill_exists = True
 
-		results = character["name"] + " rolled a " + str(roll) + " for a total result of: **" + str(total) + "**"
-		results += "\n" + stat.capitalize() + ": " + str(stat_level) + "|" + skill_str.capitalize() + ": " + str(skill_level) + "|Roll: " + str(roll) + "|LUCK: " + str(luck) + "|Mod: " + str(mod)
-		if roll == 10:
-			crit = random.randint(1, 10)
-			total += crit
-			results += "\n**CRITICAL**: Rolling...\nCrit Roll: " + str(crit) + "\nNew Total: **" + str(total) + "**"
-		if roll == 1:
-			crit = random.randint(1, 10)
-			total -= crit
-			results += "\n**CRITICAL FAILURE**: Rolling...\nCrit Roll: " + str(crit) + "\nNew Total: **" + str(total) + "**"
+        if skill_exists and skill_str in character:
+            skill_level = int(character[skill_str])
+            stat_level = int(character[stat])
+        elif skill_exists:
+            DB.collection("users").document(str(ctx.message.author.id)).collection(
+                "characters").document(active).set({skill_str: 0}, merge=True)
+            skill_level = 0
+            stat_level = int(character[stat])
+        else:
+            results = "**" + skill_str + "** is not a skill. Try again."
+            await ctx.send(results)
+            return
+        luck = 0
+        if "-l" in args:
+            flag_position = args.index("-l")
+            luck_str = str(args[flag_position + 1])
+            if luck_str.isnumeric():
+                luck = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the -l flag."
+                await ctx.send(results)
+                return
+            print(luck)
+        elif "--luck" in args:
+            flag_position = args.index("--luck")
+            luck_str = args[flag_position + 1]
+            if luck_str.isnumeric():
+                luck = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the --luck flag."
+                await ctx.send(results)
+                return
+        mod = 0
+        if "-m" in args:
+            flag_position = args.index("-m")
+            mod_str = str(args[flag_position + 1])
+            if mod_str.lstrip("-").isnumeric():
+                mod = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the -m flag."
+                await ctx.send(results)
+                return
+            print(mod)
+        elif "--mod" in args:
+            flag_position = args.index("--mod")
+            mod_str = args[flag_position + 1]
+            if mod_str.lstrip("-").isnumeric():
+                mod = int(args[flag_position + 1])
+            else:
+                results = "Please enter a number after the --mod flag."
+                await ctx.send(results)
+                return
+        roll = random.randint(1, 10)
+        total = skill_level + stat_level + roll + luck + mod
 
-		await ctx.send(results)
+        results = character["name"] + " rolled a " + \
+            str(roll) + " for a total result of: **" + str(total) + "**"
+        results += "\n" + stat.capitalize() + ": " + str(stat_level) + "|" + skill_str.capitalize() + \
+            ": " + str(skill_level) + "|Roll: " + str(roll) + \
+            "|LUCK: " + str(luck) + "|Mod: " + str(mod)
+        if roll == 10:
+            crit = random.randint(1, 10)
+            total += crit
+            results += "\n**CRITICAL**: Rolling...\nCrit Roll: " + \
+                str(crit) + "\nNew Total: **" + str(total) + "**"
+        if roll == 1:
+            crit = random.randint(1, 10)
+            total -= crit
+            results += "\n**CRITICAL FAILURE**: Rolling...\nCrit Roll: " + \
+                str(crit) + "\nNew Total: **" + str(total) + "**"
+
+        await ctx.send(results)
 
 
 def setup(bot):
-	"""
-	TODO
-	:param bot:
-	:type bot:
-	"""
-	bot.add_cog(Game(bot))
+    """
+    TODO
+    :param bot:
+    :type bot:
+    """
+    bot.add_cog(Game(bot))
